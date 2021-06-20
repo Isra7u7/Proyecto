811af9bc15e46fbe95ce77c6e8cd334cda4b2cab
Israel <ae17004@ues.edu.sv>
2021-06-20 15:59:59 -0600

    Seguridad y Permisos de vistas

--- ACPA/aplicaciones/Principal/views.py
+++ ACPA/aplicaciones/Principal/views.py
@@ -5,13 +5,14 @@ from .models import Departamento
 from .models import Entidad
 from .models import Municipio
 from .models import Paquete
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def index(request):
 	return render(request, "Principal/index.html")
 
-
+@login_required
 def lista(request):
 	beneficiarios = Beneficiario.objects.all()
 	contexto = {
@@ -19,7 +20,7 @@ def lista(request):
 	}
 	return render(request,"Principal/lista.html",contexto)
 
-
+@login_required
 def verBeneficiario(request):
 	return render(request,"Principal/verBene.html")
 
